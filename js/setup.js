d8e8a5cceb66eb39a85e065079aae42805e5d49c
Kagero <lj.chistova@yandex.ru>
2018-07-05 17:23:18 +0300

    remove hidden class setup file

---
+++ js/setup.js
@@ -0,0 +1,15 @@
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+/*var namesOfWizards = ['Иван', 'Хуан Себастьян', 'Мария',
+	 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnamesOfWizards = ['да Марья', 'Верон', 'Мирабелла',
+	 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг',];
+
+var wizardFriend = {
+	name: namesOfWizards + surnamesOfWizards;
+
+}*/
+
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
